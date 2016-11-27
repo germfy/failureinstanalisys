87cdc49ff8bb1bb09ca58445459308f375f287f6
German Dominguez <gdominguezmontes@gmail.com>
2016-11-27 13:32:59 -0600

    Complementario del 1er commit especial

---
+++ routes/servicios.js
@@ -0,0 +1,20 @@
+var watson = require('watson-developer-cloud');
+var express = require('express');
+var router = express.Router();
+var cfenv = require('cfenv');
+var Cloudant = require('cloudant');
+
+router.get('/', function(req, res, next){
+  var appEnv = cfenv.getAppEnv();
+	var alchmyService = appEnv.getService("AlchemyAPI-7v");
+
+	var alchmyUrl = appService.credentials.url;
+  var alchmyApiKey = appService.credentials.apikey;
+
+  var cldntService = appEnv.getService("gfycloudantjs-cloudantNoSQLDB");
+
+  var cldntUsername = cloudantService.credentials.username;
+  var cldntPassword = cloudantService.credentials.password;
+  var clndtHost = cloudantService.credentials.host;
+  var clndtPost = cloudantService.credentials.port;
+});
